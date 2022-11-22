f6034b0bddf32071f0a89ac21f6df1c135c7e046
Miriam <miriamjoshua00@gmail.com>
2022-11-22 00:14:46 -0500

    added User model, migration, controller

--- client/src/SignupForm.js
+++ client/src/SignupForm.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+// import LoginForm from './LoginForm';
 
 const SignupForm = () => {
 
@@ -7,33 +8,42 @@ const SignupForm = () => {
     const [passwordConfirmation, setPasswordConfirmation] = useState("")
 
   return (
-    <div>
-        <form>
-            <label>Username
-                <input 
-                type="text"
-                placeholder="Type your name here..."
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                ></input>
-            </label>
-            <label>Password
-                <input 
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                ></input>
-            </label>
-            <label>Confirm Password
-                <input 
-                type="password"
-                placeholder="Password"
-                value={passwordConfirmation}
-                onChange={(e) => setPasswordConfirmation(e.target.value)}
-                ></input>
-            </label>
-            <input type="submit"></input>
+    <div className="row container valign-wrapper">
+        <form className="col s8">
+            <div className="row">
+                <div className="input-field col s8">
+                    <label>Username </label>
+                    <input 
+                        type="text"
+                        // placeholder="Type your name here..."
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    ></input>
+                </div>
+                <div className="input-field col s8">
+                    <label>Password</label>
+                    <input 
+                        type="password"
+                        // placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    ></input>
+                </div>
+                <div className="input-field col s8">
+                    <label>Confirm Password</label>
+                    <input 
+                        type="password"
+                        // placeholder="Password"
+                        value={passwordConfirmation}
+                        onChange={(e) => setPasswordConfirmation(e.target.value)}
+                     ></input>
+                </div>
+                <div className="input-field col s8">
+                    <button className="btn waves-effect waves-light" type="submit" name="action">Sign Up
+                        <i className="material-icons right">send</i>
+                    </button>
+                </div>
+            </div>
         </form>
     </div>
   )
