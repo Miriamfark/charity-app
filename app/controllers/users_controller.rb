ed039aff5b3b49194f4603a1ab28f6528a65adab
Miriam <miriamjoshua00@gmail.com>
2022-11-23 21:40:58 -0500

    added users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,11 @@ class UsersController < ApplicationController
         render json: user, status: :created
     end
 
+    def show
+        current_user = User.find(session[:user_id])
+        render json: current_user
+    end
+
     private
 
     def user_params 
